06df3764e5e92de68a011ae534b743c961e507b4
Luis fereira <Luis@spartandevs.com>
2022-09-27 14:55:07 -0400

    Primer ejercicio de JavaScript

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,7 +2,7 @@
 // "Hola ____, tienes ____ años y el año que viene tendras ____ años"
 // Realiza el ejercicio con prompt(mensaje) y haz uso de los template strings.
 
-let nombre = prompt('Cual es tu nombre?')
-let edad = Number(prompt('Cual es tu edad?'))
+let nombre = prompt('Cual es tu nombre?');
+let edad = Number(prompt('Cual es tu edad?'));
 
-console.log(`Hola ${nombre}, tienes ${edad} años y el año que viene tendras ${edad+1} años.`)
+console.log(`Hola ${nombre}, tienes ${edad} años y el año que viene tendras ${edad+1} años.`);
